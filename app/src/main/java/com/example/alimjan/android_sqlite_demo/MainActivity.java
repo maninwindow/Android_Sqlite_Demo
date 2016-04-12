e492930beff2d069da6d75360026cefc7e33f481
Alimjan <maninwindow@hotmail.com>
2016-04-12 14:06:25 +0800

    Finalized the android SQLite demo program.

--- app/src/main/java/com/example/alimjan/android_sqlite_demo/MainActivity.java
+++ app/src/main/java/com/example/alimjan/android_sqlite_demo/MainActivity.java
@@ -3,50 +3,48 @@ package com.example.alimjan.android_sqlite_demo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText buckysInput;
+    TextView buckysText;
+    MyDBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        buckysInput = (EditText)findViewById(R.id.buckysInput);
+        buckysText = (TextView)findViewById(R.id.buckysText);
+        dbHandler = new MyDBHandler(this, null, null, 1);
+        printDatabase();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    //Add a product to the database
+    public void addButtonClicked(){
+        Products product = new Products(buckysInput.getText().toString());
+        dbHandler.addProduct(product);
+        printDatabase();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //Delete items
+    public void deleteButtonClicked(){
+        String inputText = buckysText.getText().toString();
+        dbHandler.deleteProduct(inputText);
+        printDatabase();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void printDatabase(){
+        String dbString = dbHandler.databaseToString();
+        buckysText.setText(dbString);
+        buckysInput.setText("");
     }
 }
--- app/src/main/java/com/example/alimjan/android_sqlite_demo/MyDBHandler.java
+++ app/src/main/java/com/example/alimjan/android_sqlite_demo/MyDBHandler.java
@@ -1,10 +1,71 @@
 package com.example.alimjan.android_sqlite_demo;
 
-/**
- * Created by alimjan on 4/12/16.
- */
-public class MyDBHandler {
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
+import android.content.ContentValues;
 
+public class MyDBHandler extends SQLiteOpenHelper{
 
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABcASE_NAME = "products.db";
+    public static final String TABLE_PRODUCTS = "products";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productname";
 
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super(context, DATABcASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String query = "CREATE TABLE" + TABLE_PRODUCTS + "(" +
+                COLUMN_ID + " INTEFER PRIMARY KEY AUTOINCREMENT " +
+                COLUMN_PRODUCTNAME + " TEXT " +
+                ");";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersionk, int newVersion){
+        db.execSQL("DROP_TABLE_IF_EXIST" + TABLE_PRODUCTS);
+        onCreate(db);
+    }
+
+    //Add a new row to the database
+    public void addProduct(Products product){
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCTNAME, product.getProductname());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_PRODUCTS, null, values);
+        db.close();
+    }
+
+    //Delete a product from the databse
+    public void deleteProduct(String productName){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM" + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + " =\" " + productName + "\";");
+    }
+
+    //Print out the database as a string
+    public String databaseToString(){
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE 1 ";
+
+        //Cursor point to a location in your results
+        Cursor c = db.rawQuery(query, null);
+        //Move to the first row in your results
+        c.moveToFirst();
+
+        while(!c.isAfterLast()){
+            if(c.getString(c.getColumnIndex("productname"))!=null){
+                dbString += c.getString(c.getColumnIndex("productname"));
+                dbString += "\n";
+            }
+        }
+        db.close();
+        return dbString;
+    }
 }
